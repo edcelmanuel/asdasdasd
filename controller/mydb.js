ab9fd2b113616baaab783b1d8ae748ee1d81616f
Edwin <Manuel.edwin11@gmail.com>
2021-09-05 11:18:57 +0800

    .

--- controller/mydb.js
+++ controller/mydb.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2")
 
-var pool = new mysql.createPool({
+var pool = mysql.createPool({
     connectionLimit: 100,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
